b9f4b5f8664f97cefadaba182808978532322c63
Hippy325 <garibyan.tigran2003@gmail.com>
2023-03-16 14:48:08 +0300

    refactoring(Matrix) endure algoritm

---
+++ Saper/Algoritm.swift
@@ -0,0 +1,162 @@
+//
+//  Algoritm.swift
+//  App
+//
+//  Created by User on 16.03.2023.
+//
+// swiftlint:disable cyclomatic_complexity
+
+import Foundation
+
+final class Algoritm {
+	private var number: Int = 0
+	func main(position: Int, matrix: [State]) -> Int{
+		if position < 11 {
+			leftSide(position: position, matrix: matrix)
+		}
+
+		if position > 65 {
+			rightSide(position: position, matrix: matrix)
+		}
+
+		if position <= 65 && position >= 11 {
+			centerSide(position: position, matrix: matrix)
+		}
+		return number
+	}
+
+	private func centerSide(position: Int, matrix: [State]) {
+		if matrix[position-11] == .bomb {
+			number += 1
+		}
+		if matrix[position+11] == .bomb {
+			number += 1
+		}
+
+		if position % 11 == 0 {
+			if matrix[position+1] == .bomb {
+				number += 1
+			}
+			if matrix[position+12] == .bomb {
+				number += 1
+			}
+			if matrix[position-10] == .bomb {
+				number += 1
+			}
+
+		} else {
+			if position == 65 || position == 54 || position == 43 || position == 32 || position == 21 {
+				if matrix[position-1] == .bomb {
+					number += 1
+				}
+				if matrix[position-12] == .bomb {
+					number += 1
+				}
+				if matrix[position+10] == .bomb {
+					number += 1
+				}
+
+			} else {
+				if matrix[position-1] == .bomb {
+					number += 1
+				}
+				if matrix[position+1] == .bomb {
+					number += 1
+				}
+				if matrix[position-10] == .bomb {
+					number += 1
+				}
+				if matrix[position-12] == .bomb {
+					number += 1
+				}
+				if matrix[position+12] == .bomb {
+					number += 1
+				}
+				if matrix[position+10] == .bomb {
+					number += 1
+				}
+			}
+		}
+	}
+
+	private func leftSide(position: Int, matrix: [State]) {
+		if matrix[position+11] == .bomb {
+			number += 1
+		}
+		if position == 0 {
+			if matrix[position+1] == .bomb {
+				number += 1
+			}
+
+			if matrix[position+12] == .bomb {
+				number += 1
+			}
+		} else {
+			if position == 10 {
+				if matrix[position-1] == .bomb {
+					number += 1
+				}
+
+				if matrix[position+10] == .bomb {
+					number += 1
+				}
+			} else {
+				if matrix[position-1] == .bomb {
+					number += 1
+				}
+
+				if matrix[position+10] == .bomb {
+					number += 1
+				}
+				if matrix[position+1] == .bomb {
+					number += 1
+				}
+
+				if matrix[position+12] == .bomb {
+					number += 1
+				}
+			}
+		}
+	}
+
+	private func rightSide(position: Int, matrix: [State]) {
+		if matrix[position-11] == .bomb {
+			number += 1
+		}
+
+		if position == 66 {
+			if matrix[position+1] == .bomb {
+				number += 1
+			}
+
+			if matrix[position-10] == .bomb {
+				number += 1
+			}
+		} else {
+			if position == 76 {
+				if matrix[position-1] == .bomb {
+					number += 1
+				}
+
+				if matrix[position-12] == .bomb {
+					number += 1
+				}
+			} else {
+				if matrix[position-1] == .bomb {
+					number += 1
+				}
+
+				if matrix[position-10] == .bomb {
+					number += 1
+				}
+				if matrix[position+1] == .bomb {
+					number += 1
+				}
+
+				if matrix[position-12] == .bomb {
+					number += 1
+				}
+			}
+		}
+	}
+}
--- Saper/MatrixField.swift
+++ Saper/MatrixField.swift
@@ -4,7 +4,6 @@
 //
 //  Created by User on 13.10.2022.
 //
-// swiftlint:disable cyclomatic_complexity
 
 import Foundation
 
@@ -13,12 +12,12 @@ protocol IMatrix {
 	func getMatrix() -> [Int?]
 }
 
-final class Matrix: IMatrix {
-	private enum State {
-		case bomb
-		case number
-	}
+enum State {
+	case bomb
+	case number
+}
 
+final class Matrix: IMatrix {
 	private let countBomb: Int
 	private let countCell: Int
 	private var mainMatrix: [Int?] = []
@@ -80,168 +79,12 @@ extension Matrix {
 		if matrix[position] == .bomb {
 			return nil
 		}
-
 		var number = 0
-
-		if position < 11 {
-			number += leftSide(position: position, matrix: matrix)
-		}
-
-		if position > 65 {
-			number += rightSide(position: position, matrix: matrix)
-		}
-
-		if position <= 65 && position >= 11 {
-			number += centerSide(position: position, matrix: matrix)
-		}
+		let algoritm = Algoritm()
+		number = algoritm.main(position: position, matrix: matrix)
 		return number
 	}
-
-	private func leftSide(position: Int, matrix: [State]) -> Int {
-		var number = 0
-
-		if matrix[position+11] == .bomb {
-			number += 1
-		}
-		if position == 0 {
-			if matrix[position+1] == .bomb {
-				number += 1
-			}
-
-			if matrix[position+12] == .bomb {
-				number += 1
-			}
-		} else {
-			if position == 10 {
-				if matrix[position-1] == .bomb {
-					number += 1
-				}
-
-				if matrix[position+10] == .bomb {
-					number += 1
-				}
-			} else {
-				if matrix[position-1] == .bomb {
-					number += 1
-				}
-
-				if matrix[position+10] == .bomb {
-					number += 1
-				}
-				if matrix[position+1] == .bomb {
-					number += 1
-				}
-
-				if matrix[position+12] == .bomb {
-					number += 1
-				}
-			}
-		}
-		return number
-	}
-
-	private func rightSide(position: Int, matrix: [State]) -> Int {
-		var number = 0
-
-		if matrix[position-11] == .bomb {
-			number += 1
-		}
-
-		if position == 66 {
-			if matrix[position+1] == .bomb {
-				number += 1
-			}
-
-			if matrix[position-10] == .bomb {
-				number += 1
-			}
-		} else {
-			if position == 76 {
-				if matrix[position-1] == .bomb {
-					number += 1
-				}
-
-				if matrix[position-12] == .bomb {
-					number += 1
-				}
-			} else {
-				if matrix[position-1] == .bomb {
-					number += 1
-				}
-
-				if matrix[position-10] == .bomb {
-					number += 1
-				}
-				if matrix[position+1] == .bomb {
-					number += 1
-				}
-
-				if matrix[position-12] == .bomb {
-					number += 1
-				}
-			}
-		}
-
-		return number
-	}
-
-	private func centerSide(position: Int, matrix: [State]) -> Int {
-		var number = 0
-
-		if matrix[position-11] == .bomb {
-			number += 1
-		}
-		if matrix[position+11] == .bomb {
-			number += 1
-		}
-
-		if position % 11 == 0 {
-			if matrix[position+1] == .bomb {
-				number += 1
-			}
-			if matrix[position+12] == .bomb {
-				number += 1
-			}
-			if matrix[position-10] == .bomb {
-				number += 1
-			}
-
-		} else {
-			if position == 65 || position == 54 || position == 43 || position == 32 || position == 21 {
-				if matrix[position-1] == .bomb {
-					number += 1
-				}
-				if matrix[position-12] == .bomb {
-					number += 1
-				}
-				if matrix[position+10] == .bomb {
-					number += 1
-				}
-
-			} else {
-				if matrix[position-1] == .bomb {
-					number += 1
-				}
-				if matrix[position+1] == .bomb {
-					number += 1
-				}
-				if matrix[position-10] == .bomb {
-					number += 1
-				}
-				if matrix[position-12] == .bomb {
-					number += 1
-				}
-				if matrix[position+12] == .bomb {
-					number += 1
-				}
-				if matrix[position+10] == .bomb {
-					number += 1
-				}
-			}
-		}
-		return number
-	}
-
+	
 	private func random(countC: Int, countB: Int) -> State {
 		let value = Int.random(in: 0...countC)
 		if value <= countB {
