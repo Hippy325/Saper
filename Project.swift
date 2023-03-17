b38dcb35c23c902bfdf70cdad7acfc28c982fade
Hippy325 <garibyan.tigran2003@gmail.com>
2023-03-17 16:10:33 +0300

    feature add SwiftLint

--- Project.swift
+++ Project.swift
@@ -1,5 +1,11 @@
 import ProjectDescription
 
+let swiftlintScript = """
+echo "Start Swiftlint"
+export PATH=$PATH:/opt/homebrew/bin/
+mint run swiftlint@0.46.5 swiftlint
+"""
+
 let target = Target(
 	name: "App",
 	platform: .iOS,
@@ -7,7 +13,8 @@ let target = Target(
 	bundleId: "garibyanTigran.App",
 	deploymentTarget: .iOS(targetVersion: "14.0", devices: .iphone),
 	infoPlist: "Saper/Info.plist",
-	sources: ["Saper/**"]
+	sources: ["Saper/**"],
+	actions: [.pre(script: swiftlintScript, name: "SwiftLint")]
 )
 
 let project = Project(name: "Saper", targets: [target])
